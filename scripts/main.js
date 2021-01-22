5ec7ba2421c427fb9a75c3ee33b85c533d9e611c
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-22 14:10:28 -0600

    Criminal Data function fetch(api) & imported to main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,6 @@
 console.log("Welcome to the main module")
+import {getOfficers} from './officers/OfficerProvider.js'
+import {getCriminals} from './criminals/CriminalDataProvider.js'
+
+console.log(getOfficers())
+console.log(getCriminals())
