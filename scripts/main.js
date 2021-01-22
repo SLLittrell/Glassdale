e6543ea05f4eda371c3e5a5af3a8cdf144e9ac8a
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-22 16:31:23 -0600

    Did fetch from API added into officerList.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,9 @@
 console.log("Welcome to the main module")
-import {getOfficers} from './officers/OfficerProvider.js'
-import {getCriminals} from './criminals/CriminalDataProvider.js'
+// import {getOfficers, useOfficers} from './officers/OfficerProvider.js'
+// import {getCriminals} from './criminals/CriminalDataProvider.js'
+import {officerList} from "./officers/OfficerList.js";
 
-console.log(getOfficers())
-console.log(getCriminals())
+// console.log(getOfficers())
+// console.log(getCriminals())
+
+officerList()
