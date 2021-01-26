04e4e407d4f7dfa6cfad02c7d26fcdc08c41c5be
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-26 13:24:39 -0600

    decoupling CriminalList.js and ConvictioSelect.js along with adding filter method to CriminalList.js

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -1,24 +1,68 @@
 import { getCriminals, useCriminals } from './CriminalDataProvider.js'
 import { Criminal } from "./Criminal.js";
 
-const criminalContainer = document.querySelector(".criminalsContainer")
+const contentTarget = document.querySelector(".criminalsContainer")
+const eventHub = document.querySelector(".container")
 
+// Render ALL criminals initally
 export const CriminalList = () => {
     getCriminals()
         .then(() => {
-            const criminalArray = useCriminals()
-            let criminalHTMLRep = ""
-
-            for (const criminal of criminalArray) {
-                criminalHTMLRep += Criminal(criminal)
-            }
-            criminalContainer.innerHTML = `
-            <section class="criminalList">${criminalHTMLRep}</section>
-        `
-        }
-        /*
-            Now that you have the data, what
-            component should be rendered?
-        */
-    )
+            const appStateCriminals = useCriminals()
+            render(appStateCriminals)
+        })
 }
+
+// Listen for the custom event you dispatched in ConvictionSelect
+eventHub.addEventListener("crimeChosen", event => {
+    // Use the property you added to the event detail.
+    if (event.detail.crimeThatWasChosen !== "0"){
+        /*
+            Filter the criminals application state down to the people that committed the crime
+        */
+       const appStateCriminals = useCriminals()
+        const matchingCriminals = appStateCriminals.filter(criminal =>
+            criminal.conviction === event.detail.crimeThatWasChosen
+  
+        )
+        render(matchingCriminals)
+
+        /*
+            Then invoke render() and pass the filtered collection as
+            an argument
+        */
+    }
+})
+
+const render = criminalCollection => {
+        let criminalHTMLRep = ""
+        // 
+        criminalCollection.forEach(criminal => {
+            criminalHTMLRep += Criminal(criminal)
+        });
+    contentTarget.innerHTML = `
+    <section class="criminalList">${criminalHTMLRep}</section>
+`
+}
+
+
+
+// export const CriminalList = () => {
+//     getCriminals()
+// //         .then(() => {
+//             const criminalArray = useCriminals()
+//             let criminalHTMLRep = ""
+
+//             for (const criminal of criminalArray) {
+//                 criminalHTMLRep += Criminal(criminal)
+//             }
+//             criminalContainer.innerHTML = `
+//             <section class="criminalList">${criminalHTMLRep}</section>
+//         `
+//         }
+//         /*
+//             Now that you have the data, what
+//             component should be rendered?
+//         */
+//     )
+// }
