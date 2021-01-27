d968217bd02029225eb27c9cb1c56e181889a152
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-27 11:42:49 -0600

    added officer dropdown in OfficerSelect.js redered to html and sent to DOM

--- scripts/officers/OfficerList.js
+++ scripts/officers/OfficerList.js
@@ -1,23 +1,22 @@
-import { getOfficers, useOfficers } from "./OfficerProvider.js";
-import {Officer} from './Officer.js'
+// import { getOfficers, useOfficers } from "./OfficerDataProvider.js";
 
-const officerContainer = document.querySelector(".officerContainer")
+// const officerContainer = document.querySelector(".officerContainer")
 
-export const OfficerList = () => {
-    getOfficers()
-        .then(() => {
-            const officerArray = useOfficers()
+// export const OfficerList = () => {
+//     getOfficers()
+//         .then(() => {
+//             const officerArray = useOfficers()
        
-        let officerHtmlRep = ""
+//         let officerHtmlRep = ""
 
-        for (const officer of officerArray) {
-            officerHtmlRep += Officer(officer)
+//         for (const officer of officerArray) {
+//             officerHtmlRep += Officer(officer)
 
-        }
+//         }
 
-        officerContainer.innerHTML = `
-            <h3>Glassdale Officers</h3><section class="officerList">${officerHtmlRep}</section>
-        `
-    })
-}
+//         officerContainer.innerHTML = `
+//             <h3>Glassdale Officers</h3><section class="officerList">${officerHtmlRep}</section>
+//         `
+//     })
+// }
 
---
+++ scripts/officers/OfficerSelect.js
@@ -0,0 +1,53 @@
+import { getOfficers, useOfficers } from "./OfficerDataProvider.js"
+const contentTarget = document.querySelector(".filters__officer")
+// eventHub.addEventListener("change", changeEvent => {
+//     if (changeEvent.target.id === "officerSelect") {
+//         // Get the name of the selected officer
+//         const selectedOfficer = changeEvent.target.value
+
+//         // Define a custom event
+//         const customEvent = new CustomEvent("officerSelected", {
+//             detail: {
+//                 officer: selectedOfficer
+//             }
+//         })
+
+//         // Dispatch event to event hub
+//         eventHub.dispatchEvent(customEvent)
+//     }
+// })
+
+export const OfficerSelect = () => {
+    // Trigger fetching the API data and loading it into application state
+    getOfficers()
+    .then( () => {
+      // Get all convictions from application state
+      const officerArray = useOfficers()
+      render(officerArray)
+    })
+}
+
+
+const render = officerCollection => {
+    /*
+    Use interpolation here to invoke the map() method on
+    the officerCollection to generate the option elements.
+    */
+   contentTarget.innerHTML = `
+   <select class="dropdown" id="officerSelect">
+        <option value="0">Officers</option>
+         ${
+            officerCollection.map(officerObject => 
+            `<option value="${officerObject.id}">${officerObject.name}</option>`) .join("")
+            }
+        </select>
+    `
+}
+
+
+
+// export const Officer = (officerObj) => {
+//     return `
+//     <p class="officer>${officerObj.name}</p>
+//     `
+// 
