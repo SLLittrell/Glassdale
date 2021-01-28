6a320d0c13a94ba576164807b7a561b2b578d729
Stacey Littrell <stacey.littrell@gmail.com>
2021-01-28 10:40:48 -0600

    added some styling to NoteForm

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -2,7 +2,7 @@ export const NoteHTMLConverter = (noteObject) => {
     return `
         <section class="note">
             <div class="note__text">${ noteObject.note }</div>
-            <div class="note__suspect">Title: ${ noteObject.suspect }</div>
+            <div class="note__suspect">Suspects Name: ${ noteObject.suspect }</div>
             <div class="note__timestamp">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
         </section>
     `
