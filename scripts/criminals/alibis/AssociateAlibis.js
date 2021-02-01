805f208a43099e17fee54c8cb8ddc64cf9c6d803
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-01 13:00:17 -0600

    Associate heading

--- scripts/criminals/alibis/AssociateAlibis.js
+++ scripts/criminals/alibis/AssociateAlibis.js
@@ -5,9 +5,20 @@ import { Alibi } from "./Alibis.js";
 const eventHub = document.querySelector(".container")
 const contentTarget = document.querySelector(".dialog")
 
-// export const ShownAssociates = (criminalObj) => {
-//     console.log(criminalObj)
-// }
+export const ShownAssociates = (criminalObj) => {
+    const htmlHead = `<h3>${criminalObj.name}'s Associates and Alibi's</h3>`
+    const alibiChosen = criminalObj.known_associates.map( associates => {
+        const htmlRep =  `
+        <section class="associate_container">
+        <div>Name: ${associates.name}</div>
+        <div>Alibi: ${associates.alibi}</div>
+        </section>`
+        return htmlRep
+    })
+    
+    contentTarget.innerHTML = htmlHead + alibiChosen
+
+}
 
 //listens for click on associate Alibi button
 eventHub.addEventListener("chosenAlibi", customEvent => {
@@ -19,45 +30,19 @@ eventHub.addEventListener("chosenAlibi", customEvent => {
     const criminalAlibi = alibiArray.find(alibiObj => {
         return alibiObj.id === parseInt(customEvent.detail.alibiThatWasChosen)
     })
+    ShownAssociates(criminalAlibi)
     // console.log(criminalAlibi)
-    const alibiChosen = criminalAlibi.known_associates.map( associates => {
-        const associateName =  `
-        <h3>${criminalAlibi.name}'s Associates and Alibi's</h3>
-        <section class="associate_container">
-        <div>Name: ${associates.name}</div>
-        <div>Alibi: ${associates.alibi}</div>
-        </section>`
-        return associateName
-    })
+    // const alibiChosen = criminalAlibi.known_associates.map( associates => {
+    //     const associateName =  `
+    //     <h3>${criminalAlibi.name}'s Associates and Alibi's</h3>
+    //     <section class="associate_container">
+    //     <div>Name: ${associates.name}</div>
+    //     <div>Alibi: ${associates.alibi}</div>
+    //     </section>`
+    //     return associateName
+    // })
     
-    contentTarget.innerHTML = alibiChosen
+    // contentTarget.innerHTML = alibiChosen
 
 
-            
-
-            
-            
-            
-            // contentTarget.innerHTML += `
-            // <dialog class="example-dialog">
-            //     <div class="alibi--name">name: ${alibiChosen.name}</div>
-            //     <div class="alibi--text">alibi: ${alibiChosen.alibi}</div>
-            //     <button class="close" type="reset">Close</button>
-            // </dialog>
-        
-            // `
-            
-        // }
-        //     // console.log("click heard")
-    
-    
-    
-    
-    // export const AlibiBox = () => {
-        // getCriminals()
-        // .then(() => {
-        //     const criminals= useCriminals()
-        //     render(criminals)
-        // })
-    
 })
