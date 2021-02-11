ee89ab49087b3f3c60490b3f8400a09492dcfb6d
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-11 13:24:28 -0600

    added criminal Id to note form

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -2,7 +2,7 @@ export const NoteHTMLConverter = (noteObject) => {
     return `
         <section class="note">
             <div class="note__text">Note:${ noteObject.note }</div>
-            <div class="note__suspect">Suspects Name: ${ noteObject.suspect }</div>
+            <div class="note__auther">Officer Name: ${ noteObject.suspect }</div>
             <div class="note__timestamp">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
         </section>
     `
