f4cbb994f62568058ba745e8b4be3f690c7cd47d
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-11 16:20:45 -0600

    fixed note ouput

--- scripts/notes/Note.js
+++ scripts/notes/Note.js
@@ -1,8 +1,9 @@
-export const NoteHTMLConverter = (noteObject) => {
+export const NoteHTMLConverter = (noteObject, criminalObject) => {
     return `
         <section class="note">
-            <div class="note__text">Note:${ noteObject.note }</div>
-            <div class="note__auther">Officer Name: ${ noteObject.suspect }</div>
+            <div class="note__suspect">Note about: ${criminalObject.name}</div>
+            <div class="note__text">Note: ${ noteObject.note }</div>
+            <div class="note__auther">Officer Name: ${ noteObject.auther }</div>
             <div class="note__timestamp">Timestamp: ${ new Date(noteObject.date).toLocaleDateString('en-US')  }</div>
         </section>
     `
