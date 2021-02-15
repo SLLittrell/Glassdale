843aeddc0b9fa2b6bdfe0e51f1d5f2ac51213b3a
Stacey Littrell <stacey.littrell@gmail.com>
2021-02-15 12:07:22 -0600

    rendered facilities with associated glassdale criminals to DOM

--- scripts/facility/Facility.js
+++ scripts/facility/Facility.js
@@ -1,10 +1,10 @@
-export const Facility = (facility) => {
+export const Facility = (facility, criminals) => {
     return `
        <div class="facility">
         <h2>${facility.facilityName}</h2>
         <div>Security: ${facility.securityLevel}</div>
         <div>Capacity: ${facility.capacity}</div>
-        <div>Criminals: </div>
+        <div>Criminals:  ${criminals.map(c => `<li>${c.name}</li>`).join("")}</div>
        </div>
     `
 }
--- scripts/facility/FacilityList.js
+++ scripts/facility/FacilityList.js
@@ -1,4 +1,5 @@
 import {Facility} from "../facility/Facility.js"
+import {getCriminals, useCriminals} from "../criminals/CriminalDataProvider.js"
 import { getFacilities, useFacilities } from '../facility/FacilityProvider.js';
 import { getCriminalFacilities, useCriminalFacilities } from '../facility/CriminalFacilityProvider.js';
 
@@ -8,21 +9,40 @@ const eventHub = document.querySelector(".container")
 
 export const FaciltyList= () => {
     getFacilities()
-    .then(() => {
-        const facilityArray = useFacilities()
-        renderFacility(facilityArray)
-    })
+    .then(getCriminalFacilities)
+        .then(getCriminals)
+        .then(() => {
+            // Pull in the data now that it has been fetched
+            const facilityArray = useFacilities()
+            const crimFacility = useCriminalFacilities()
+            const criminals = useCriminals()
     
-}
-
-const renderFacility = (facilityObject) => {
-    let faciltyhtmlRep = ""
-    for (const facility of facilityObject ) {
-        faciltyhtmlRep += Facility(facility)
+        renderFacility(facilityArray,crimFacility,criminals)
+    })
+    const renderFacility=(allFacilities, allRelationships, criminalArray) => {
+        // Step 1 - Iterate all facility
+        const facilityFilter = allFacilities.map(
+            (facilityObject) => {
+                // Step 2 - Filter all relationships to get only ones for this facility
+                const CriminalRelationshipsForThisFacility = allRelationships.filter(cf => cf.facilityId === facilityObject.id)
+    
+                // Step 3 - Convert the relationships to criminals with map()
+                const criminal = CriminalRelationshipsForThisFacility.map(cf => {
+                    const matchingCriminalObject = criminalArray.find(criminals => criminals.id === cf.criminalId)
+                    return matchingCriminalObject
+                })
+    
+                // Must pass the matching Criminal to the facilities component
+            //    console.log(facilityObject, criminal)
+                return Facility(facilityObject, criminal)
+            }
+        ).join("")
+    
+        contentTarget.innerHTML = ` <section class="facilityList">${facilityFilter} </section>`
     }
-
-    contentTarget.innerHTML = ` <section class="facilityList">${faciltyhtmlRep} </section>`
 }
+
+
 eventHub.addEventListener("facilitiesButtonClicked", customEvent => {
     FaciltyList()
     
